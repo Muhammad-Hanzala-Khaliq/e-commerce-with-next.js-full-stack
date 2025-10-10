94d24cf5c42375b75f9f9b3a94b7208df3fdf8f7
M.Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2025-10-11 00:27:23 +0500

    feat: build homepage layout and add responsive header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import Container from './Container'
+import Logo from "./Logo";
+import HeaderMenu from "./HeaderMenu";
+import SearchBar from "./SearchBar";
+import CartIcon from "./CartIcon";
+import FavouriteButton from "./FavouriteButton";
+import SignIn from "./SignIn";
+import MobileMenu from "./MobileMenu";
 
 const Header = () => {
   return (
-    <header>
-      <Container className=''>
-        Header
+    <header className="bg-white py-5 border-b border-b-black/20 ">
+      <Container className="flex items-center justify-between text-lightColor">
+        {/* Logo */}
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+          <MobileMenu />
+          <Logo />
+        </div>
+        {/* NavButton */}
+        <HeaderMenu />
+
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavouriteButton />
+          <SignIn />
+        </div>
+        {/* Navadmin */}
       </Container>
     </header>
-  )
-}
+  );
+};
 
 export default Header
